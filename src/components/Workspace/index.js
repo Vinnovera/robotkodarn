baa0e093f6a3e38258988e5f5877b29b25a80993
Christian Alares <christian.alares@gmail.com>
2017-11-27 12:23:19 +0100

    Extracted Workspace Buttobs from the Workspace component to its own

--- src/components/Workspace/index.js
+++ src/components/Workspace/index.js
@@ -1,37 +1,23 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import FA from 'react-fontawesome'
-
-import {
-  compileCode,
-  setConsoleOutput,
-  toggleCodeButtons,
-  pingChromeApp,
-  pingForUSBConnection,
-  animateCompileButton,
-  animateUploadButton
-} from '../../actions/editor'
 
 import { findWorkshopByPin, clearWorkshop } from '../../actions/currentWorkshop'
 import Sidebar from './../Sidebar'
 import Editor from './../Editor'
 import WorkspaceForm from './../WorkspaceForm'
 import Console from './../Console'
-import Button from './../Button'
 import Spinner from './../Spinner'
 import View from './../View'
 import FadeIn from './../FadeIn'
 import ToolsButton from './../ToolsButton'
 
-import CogIcon from '../Icons/CogIcon'
-import USBIcon from '../Icons/USBIcon'
+import WorkspaceButtons from './WorkspaceButtons'
 
 import styles from './workspace.css'
 
 export class Workspace extends Component {
   componentWillMount() {
     this.props.dispatch(findWorkshopByPin(this.props.params.pin))
-    this.ping(1000)
   }
 
   componentWillUnmount() {
@@ -44,40 +30,6 @@ export class Workspace extends Component {
     }
 
     return `${styles.mainPane} ${styles.mainPaneExpanded}`
-  }
-
-  ping(interval) {
-    setInterval(() => {
-      this.props.dispatch(pingChromeApp())
-      this.props.dispatch(pingForUSBConnection())
-    }, interval)
-  }
-
-  /**
-   * Gives user message about compiling is taking place, and also dispatch the
-   * code that is about to be compiled. If upload is set to true, code will also
-   * be uploaded to Arduino.
-   *
-   * @param {boolean} upload true = code will be uploaded to Arduino
-   */
-  handleClick = (upload = false) => {
-    this.props.dispatch(setConsoleOutput({
-      type: 'info',
-      heading: 'Testar kod',
-      message: 'Skickar kod till kompilator...'
-    }))
-
-    this.props.dispatch(
-      compileCode(this.props.partsToEdit[this.props.activePartIndex].content, upload)
-    )
-
-    if (upload) {
-      this.props.dispatch(animateUploadButton(true))
-    } else {
-      this.props.dispatch(animateCompileButton(true))
-    }
-
-    this.props.dispatch(toggleCodeButtons(false))
   }
 
   renderMainContent() {
@@ -100,41 +52,7 @@ export class Workspace extends Component {
                   :
                   <h1 className={styles.workspaceHeadline}>Övning</h1>
                 }
-                {
-                  this.props.enabledButtons
-                    ? (
-                      <div className={styles.codeButtonsWrapper} >
-                        <Button kind="success" handleClick={() => this.handleClick()}>
-                          <CogIcon className={styles.cogwheelIcon} />Testa min kod
-                        </Button>
-
-                        {
-                          this.props.chromeAppReachable && this.props.deviceConnected
-                            ? (
-                              <Button kind="success" handleClick={() => this.handleClick(true)}>
-                                <USBIcon className={styles.usbIcon} />Ladda över kod
-                              </Button>
-                            )
-                            : (
-                              <Button kind="disabled">
-                                <USBIcon className={styles.usbIcon} />Ladda över kod
-                              </Button>
-                            )
-                        }
-                      </div>
-                    )
-                    : (
-                      <div className={styles.codeButtonsWrapper} >
-                        <Button kind="disabled">
-                          <CogIcon className={`${styles.cogwheelIcon} ${this.props.animatedCompileButton ? styles.compileButtonIsAnimated : ''}`} />Testa min kod
-                        </Button>
-
-                        <Button kind="disabled">
-                          <USBIcon className={`${styles.usbIcon} ${this.props.animatedUploadButton ? styles.uploadButtonIsAnimated : ''}`} />Ladda över kod
-                        </Button>
-                      </div>
-                    )
-                }
+                <WorkspaceButtons />
                 <Editor />
                 <Console />
               </main>
@@ -162,12 +80,7 @@ function mapStateToProps(state) {
     editing: state.editor.editing,
     editingType: state.editor.editingType.type,
     partsToEdit: state.editor.partsToEdit,
-    isLoggedIn: state.user.isLoggedIn,
-    enabledButtons: state.editor.enabledButtons,
-    chromeAppReachable: state.editor.chromeAppReachable,
-    deviceConnected: state.editor.deviceConnected,
-    animatedCompileButton: state.editor.animatedCompileButton,
-    animatedUploadButton: state.editor.animatedUploadButton
+    isLoggedIn: state.user.isLoggedIn
   }
 }
 
